fb5a84fb886105c14c5decfc045fde74c98e1b48
Konstantin Protzen <cuzimbisonratte@gmail.com>
2023-07-13 21:33:29 +0200

    Update title

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once("no-github.php");
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Konstantin Protzen</title>
+    <title>Konstantin Protzen | Webdevelopment und Design - made in Germany</title>
     <!-- Stylesheets -->
     <link rel="stylesheet" href="/res/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="/res/fontawesome/css/brands.min.css">
