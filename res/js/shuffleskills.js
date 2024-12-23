d06aab0f4e3cf383248049cb68efe567c98dfd19
Konstantin Protzen <code@kondev.de>
2024-12-23 23:32:01 +0100

    🎉 Remove everything for page redesign

--- res/js/shuffleskills.js
+++
@@ -1,5 +0,0 @@
-function shuffleSkills() {
-    const e = document.querySelectorAll(".skill_list-wrapper>p");
-    for (let i = 0; e.length > i; i++) e[i].style.order = Math.floor(Math.random() * e.length);
-}
-shuffleSkills();
