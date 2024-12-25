3f64a778642b24b4b5070ce8987a140c8738d28c
Konstantin Protzen <code@kondev.de>
2024-12-25 17:10:42 +0100

    🧑‍💻 Improve debug script

--- res/js/debug.js
+++ res/js/debug.js
@@ -2,34 +2,51 @@ let debugTyped = "";
 let debugTimeout;
 let debug;
 
-document.addEventListener("keydown", function(event) {
+function debug_clear() {
+    let elements = document.querySelectorAll("*");
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.border = "";
+        debug = false;
+    }
+}
+
+function debug_start() {
+    let elements = document.querySelectorAll("*");
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.border = "1px solid red";
+        elements[i].style.boxSizing = "border-box";
+        debug = true;
+    }
+}
+
+document.addEventListener("keydown", function (event) {
     debugTyped += event.key.toLowerCase();
     if (debugTyped === "debug") {
-        let elements = document.querySelectorAll("*");
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].style.border = "1px solid red";
-            elements[i].style.boxSizing = "border-box";
-            debug = true;
-        }
+        debug_start();
     } else if (debugTyped.length > 5 && event.key.toLowerCase() == "d") {
         debugTyped = "d";
     }
 });
 
-document.addEventListener("keyup", function(event) {
-    if (event.key === "Escape") {
-        let elements = document.querySelectorAll("*");
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].style.border = "";
-            debug = false;
-        }
-    }
+document.addEventListener("keyup", function (event) {
+    if (event.key === "Escape") debug_clear();
 });
 
-document.addEventListener("keydown", event => { if (event.key.toLowerCase() == "t") debugCycle() });
-
-function debugCycle() {
-    if (!debug) return;
-    cycleTheme();
-    window.setTimeout(debugCycle, 1000);
-}
+lastTouchTime = 0;
+touches = 0;
+lastTouch = [0, 0];
+document.addEventListener("touchstart", function (event) {
+    if (Date.now() - lastTouchTime > 1000) touches = 0;
+    lastTouchTime = Date.now();
+    // Check if the touch is near the last touch - within 10px
+    if (Math.abs(event.touches[0].clientX - lastTouch[0]) < 25 && Math.abs(event.touches[0].clientY - lastTouch[1]) < 25) {
+        touches++;
+    } else {
+        touches = 0;
+    }
+    lastTouch = [event.touches[0].clientX, event.touches[0].clientY];
+    if (touches >= 5) {
+        debug ? debug_clear() : debug_start();
+        touches = 0;
+    }
+});
